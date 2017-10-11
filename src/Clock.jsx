f4c8475848ec6ea41461dcf2db41fbdfd8b974e4
HAFIZBILALSAEED\Hafiz Bilal Saeed <bilal.saeed@techlogix.com>
2017-10-11 18:16:05 +0500

    New Changes committed from Office Machine

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -15,6 +15,11 @@ class Clock extends Component {
   getTimeUntil(deadline){
     const time= Date.parse(deadline) - Date.parse(new Date());
     console.log(time);
+    const seconds = Math.floor((time/1000) % 60);
+    const minutes = Math.floor((time/1000/60) % 60);
+    const hours = Math.floor(time/(1000*60*60) % 24);
+    const days = Math.floor(time/(1000*60*60*24));
+
   }
 
   render() {
